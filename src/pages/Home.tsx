b99ee0d61255a00b9bdc6d46f5242d5a6fb62d89
chillout2san <75463077+chillout2san@users.noreply.github.com>
2022-03-05 15:30:39 +0900

    feat: routerの設定。prettierの設定。

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home: React.VFC = () => {
+  return <div>Homeコンポーネント</div>;
+};
+
+export default Home
---
+++ src/pages/Work.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Work: React.VFC = () => {
+  return <div>Workコンポーネント</div>
+}
+
+export default Work
